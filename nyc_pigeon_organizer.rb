c4804d37ed012ba35851f0cfe24d022304d7a65d
Don Mallory <dmm333@gmail.com>
2020-02-22 04:18:15 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,12 +1,7 @@
 def nyc_pigeon_organizer(data)
-  newHash = {}
-  data.each_key {|attribute|
-    puts "*********"
-    puts attribute.size
-    attribute.each_key {|value|
-      puts "****************"
-      pp value
-    }
+  newHash = data.reduce({}) {|newHash, element|
+    puts element
+
   }
 
   newHash
