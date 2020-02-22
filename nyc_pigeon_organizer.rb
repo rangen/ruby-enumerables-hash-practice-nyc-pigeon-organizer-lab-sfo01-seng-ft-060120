34293a1cca8f0217889b7eae42ce1761df373f16
Don Mallory <dmm333@gmail.com>
2020-02-22 05:58:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,9 +3,11 @@ newHash = Hash.new
   data.each_pair {|attribute, value|
     value.each_pair {|value, owner|
         owner.each {|name|
-          #newHash[name][attribute.to_s] <<
-          puts "#{value}   #{attribute}  #{name}"
+          newHash[name] = Hash.new if !newHash[name]
+          newHash[name][attribute] = Hash.new if !newHash[name][attribute]
+          newHash[name][attribute] << value
         }
     }
   }
+  newHash
 end
